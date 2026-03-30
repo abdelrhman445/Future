efea3edc350b585f069f8b13f3581ecd1fab84db
Abdulrahman Elbatea <139005150+abdelrhman445@users.noreply.github.com>
2026-03-30 22:23:19 +0200

    Update courses.router.ts

--- Future_Backend/src/modules/courses/courses.router.ts
+++ Future_Backend/src/modules/courses/courses.router.ts
@@ -7,8 +7,6 @@ import prisma from '../../config/prisma';
 
 const router = Router();
 
-
-
 function handleValidation(req: Request, _res: Response, next: NextFunction): void {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return next(new AppError(422, errors.array()[0].msg));
